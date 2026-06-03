9d0284fe72370a9f95ffda6bf632441b35299720
BetterGrumpyJake <139168180+BetterGrumpyJake@users.noreply.github.com>
2026-06-02 20:52:57 -0800

    remove spawned from block
    removes the block the vine spawns from

--- PRG/prg001.asm
+++ PRG/prg001.asm
@@ -1024,6 +1024,7 @@ Bounce_TileReplacements:
 	.byte CHNGTILE_TOBOUNCEWOOD
 	.byte CHNGTILE_TOBRICKCOIN
 	.byte CHNGTILE_PIPEJCT
+	.byte CHNGTILE_DELETETOBG
 
 	; Power-up X or Y velocity upon emerging from the bounce block
 Bouncer_PUpVel:	.byte $00, -$40, -$40, -$30, -$20, -$10, $00, $10, $20, $30, $40
--- PRG/prg008.asm
+++ PRG/prg008.asm
@@ -4861,7 +4861,7 @@ LATR_GNote:	.byte $00
 LATR_HNote:	.byte $10
 LATR_Notes:	.byte $40, $40, $40, $40
 LATR_Woodblocks:.byte $50, $50, $50, $50
-LATR_QBlocks:	.byte $20, $20, $20, $20, $20, $20, $20, $30, $20, $20, $20, $20, $20, $60, $20, $20, $20
+LATR_QBlocks:	.byte $20, $20, $20, $20, $20, $20, $20, $30, $20, $20, $20, $20, $20, $60, $20, $80, $20
 LATR_InvisCoin:	.byte $20, $20, $10
 LATR_InvisNote:	.byte $40
 LATR_PWrksJct:	.byte $70	; UNUSED breakable pipeworks junction tile!

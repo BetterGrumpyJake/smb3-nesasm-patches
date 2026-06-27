6df0850298290b691058d918198f01525cd2b34f
BetterGrumpyJake <139168180+BetterGrumpyJake@users.noreply.github.com>
2026-06-27 08:07:51 -0800

    bully
    credit-russianman

--- PRG/prg000.asm
+++ PRG/prg000.asm
@@ -295,7 +295,7 @@ Object_BoundBox:
 
 Object_AttrFlags:
 	; Defines flags which set attributes of objects
-	.byte OAT_BOUNDBOX02 | OAT_WEAPONIMMUNITY | OAT_FIREIMMUNITY | OAT_HITNOTKILL	; Object $00
+	.byte OAT_BOUNDBOX01 | OAT_FIREIMMUNITY | OAT_HITNOTKILL	; Object $00
 	.byte OAT_BOUNDBOX01 | OAT_FIREIMMUNITY | OAT_HITNOTKILL	; Object $01
 	.byte OAT_BOUNDBOX01 | OAT_FIREIMMUNITY | OAT_HITNOTKILL	; Object $02
 	.byte OAT_BOUNDBOX00	; Object $03

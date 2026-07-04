6215c788818997b21e9ee63f9bb716682bec4417
BetterGrumpyJake <139168180+BetterGrumpyJake@users.noreply.github.com>
2026-07-03 23:47:02 -0800

    boot on stomp

--- PRG/prg002.asm
+++ PRG/prg002.asm
@@ -3519,7 +3519,14 @@ PRG002_B119:
 	JMP Player_GetHurt ; Hurt Player and don't come back!
 
 PRG002_B126:
-	JMP PRG000_D2B4	 ; Jump to PRG000_D2B4 (hijacks the kill routine)
+	JSR PRG000_D2B4 ; Jump to PRG000_D2B4 (hijacks the kill routine)
+	LDA #OBJSTATE_NORMAL
+	STA Objects_State,X
+	LDA #$0e
+	STA <Objects_Var5,X
+	;LDA #-$40
+	;STA <Player_YVel	
+	RTS
 
 	; The Y offset for the Goomba riding in the shoe
 Shoe_GoombaYOff:
@@ -6328,8 +6335,8 @@ PRG002_BFD3:
 	RTS		 ; Return
 
 	; ?? Someone wanna claim this?
-PRG002_BFD4:
-	.byte $FC, $A9, $00, $22, $0B, $01, $A9, $22, $14, $01, $A9, $22, $29, $04, $A9, $FC
-	.byte $FC, $A9, $22, $33, $04, $A9, $FC, $FC, $A9, $22, $4A, $04, $A9, $A9, $FC, $A9
-	.byte $22, $52, $04, $A9, $FC, $A9, $A9, $22, $6C, $48, $A9, $00
+;PRG002_BFD4:
+;	.byte $FC, $A9, $00, $22, $0B, $01, $A9, $22, $14, $01, $A9, $22, $29, $04, $A9, $FC
+;	.byte $FC, $A9, $22, $33, $04, $A9, $FC, $FC, $A9, $22, $4A, $04, $A9, $A9, $FC, $A9
+;	.byte $22, $52, $04, $A9, $FC, $A9, $A9, $22, $6C, $48, $A9, $00
 
